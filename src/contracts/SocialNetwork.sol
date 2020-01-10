bee2fdb70118893b673029be31ded5db16b76031
ishant.garg <ishant.garg@daffodilsw.com>
2020-01-10 17:45:32 +0530

    some changes

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -29,7 +29,7 @@ contract SocialNetwork {
   );
 
   constructor() public {
-    name = "Dapp University Social Network";
+    name = "My Social Network";
   }
    function createPost(string memory _content) public {
       //validate the content string
